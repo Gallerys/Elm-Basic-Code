d05dab4972a40941923410ab13dcb80e8941733b
Code <59381987+c-o-de@users.noreply.github.com>
2023-08-22 08:59:22 +0600

    Add files via upload

---
+++ main.elm
@@ -0,0 +1,9 @@
+-- This is a comment in Elm. It's ignored by the Elm compiler and used for documentation.
+
+-- Import the necessary module for working with HTML
+import Html exposing (text)
+
+-- Define the main program
+main =
+    -- Render the text "Hello, Elm!" as the main content
+    text "Hello, Elm!"
